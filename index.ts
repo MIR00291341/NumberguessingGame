052955d8752337286c9c0d8d3394a1eb1481ffbe
Muhammad Ismail Rasheed <159624882+MIR00291341@users.noreply.github.com>
2024-03-28 19:53:31 +0500

    main
    Number Guessing Game By M Ismail Rasheed.

---
+++ index.ts
@@ -0,0 +1,12 @@
+import inquirer from "inquirer";
+const randomNumber = 12
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please Guess A Number"
+    }])
+    if (answers.userGuessedNumber === randomNumber)
+{console.log("Congratulations! You gussed right number.");
+}
+else {console.log("Sorry! You gussed wrong number.")};
